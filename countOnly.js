d8c8368a45f1a68738dd2da56aaec166e46eff40
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-21 16:41:29 -0400

    corrected some mistakes

--- countOnly.js
+++ countOnly.js
@@ -42,7 +42,7 @@ const firstNames = [
 ];
   
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
- r
+
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
