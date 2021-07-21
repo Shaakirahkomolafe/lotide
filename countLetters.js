5768b3fa290f08780c5c1156be6ee6b015070351
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-21 18:20:39 -0400

    ne function for counting letters

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed : ${actual} === ${expected}`);
+  } else {
+    console.log(` 🔴🔴🔴 Assertion failed : ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(allString) {
+  let results = {};
+  for (let letter of allString) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+      console.log(letter);
+    }
+  }
+  return results;
+};
+console.log(countLetters("lighthouse in the house"));
