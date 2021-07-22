1f02f3a4614da9d6040b9f78f34d85666262c303
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-22 07:58:53 -0400

    corrections

--- assertEqualArrays.js
+++ assertEqualArrays.js
@@ -12,11 +12,12 @@ const eqArrays = function(arrayOne, arrrayTwo) {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (arr1 === arr2) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion passed : ${arr1} === ${arr2}`);
   } else {
     console.log(` 🔴🔴🔴 Assertion failed : ${arr1} !== ${arr2}`);
   }
+  return assertArraysEqual
 };
 
 assertArraysEqual("Lighthouse Labs", "Bootcamp");
