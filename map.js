8ed15ddb9fe09805435cd931d9d95883f8c8e3cb
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-22 20:32:50 -0400

    map function for lotide

---
+++ map.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arrayOne, arrrayTwo) {
+  if (arrayOne.length !== arrrayTwo.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion passed : ${arr1} === ${arr2}`);
+  } else {
+    console.log(` 🔴🔴🔴 Assertion failed : ${arr1} !== ${arr2}`);
+  }
+  return assertArraysEqual
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+
+
+assertArraysEqual(map(words, word => word[word.length - 1]), ['d', 'l', 'o', 'r', 'm']);
+
+
