6f7f0307b22c1679dc55baf792fbad3ba55f7074
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-23 08:09:20 -0400

    function to findKey

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed : ${actual} === ${expected}`);
+  } else {
+    console.log(` 🔴🔴🔴 Assertion failed : ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let item of Object.keys(object)) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), "Blue Hill");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "Akaleri");
+
