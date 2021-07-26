f789a177dc7ef16adb0e53299e2d8986acf372f4
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-26 13:26:24 -0400

    making the tail.js file of lotide dryer

--- tail.js
+++ tail.js
@@ -1,19 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed : ${actual} === ${expected}`);
-  } else {
-    console.log(` 🔴🔴🔴 Assertion failed : ${actual} !== ${expected}`);
-  }
-};
- 
+const assertEqual = require('./assertEqual');
+
+
+
 const tail = function(array) {
   let num = [];
-  for (i = 0; i < array.length; i++) {
-    return array.shift();
+  for (let i = 0; i < array.length; i++) {
+   return array.shift();
   }
+
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
